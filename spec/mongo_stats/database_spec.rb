f9225d026468586f34e4aa840d9e4a69d27a1bcc
Julian Russell <jr.plusplus@gmail.com>
2013-02-07 15:56:41 +1100

    Added smoke test for database

--- spec/mongo_stats/database_spec.rb
+++ spec/mongo_stats/database_spec.rb
@@ -2,19 +2,17 @@ require 'spec_helper'
 require 'mongo_stats/update_converter'
 describe MongoStats::Database do
 
-  describe "#new" do
 
-    let(:mongo_database) {double(:mongo_db)}
-    let(:periods) {double(:periods)}
-    let(:prefix) {"prefix"}
+  let(:mongo_database) {double(:mongo_db)}
+  let(:periods) {double(:periods)}
+  let(:prefix) {"prefix"}
+  subject {MongoStats::Database.new( mongo_db: mongo_database, periods: periods, mongo_collection_prefix: prefix )}
 
-    it "accepts right attributes" do
-      MongoStats::Database.new( mongo_db: mongo_database, periods: periods, mongo_collection_prefix: prefix )
-    end
 
-    it "can have collection called on it" do
-      collection = MongoStats::Database.new( mongo_db: mongo_database, periods: periods, mongo_collection_prefix: prefix ).collection("foo")
-
+  describe "#collection" do
+    let( :collection ) {subject.collection("foo")}
+    
+    it "passes everything to the collection" do
       collection.mongo_database.should be(mongo_database)
       collection.periods.should be(periods)
       collection.name.should eq("foo")
